0aef44239aa9c709e82cc3d6045d6d28d25f083d
Dirk Meihoefer <dirk.meihoefer@wago.com>
2022-06-13 14:26:07 +0200

    Added pulling of certs.

--- build-context/greengrass-entrypoint.sh
+++ build-context/greengrass-entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+#Only necessary for use behind a corporate firewall
+curl -ksSL $(curl -ksSL $(curl -ksSL https://artifactory.wago.local/api/storage/certs-generic-prod-local/wago-intercept-certificates?lastModified | jq -r .'uri') | jq -r .'downloadUri') -o /usr/local/share/ca-certificates/wago.crt; exit 0
+update-ca-certificates
+
 /greengrass/ggc/core/greengrassd start
 
 daemon_pid=`cat /var/run/greengrassd.pid`
@@ -10,7 +14,7 @@ while [ -d /proc/$daemon_pid ]
 do
  # Sleep for 1s before checking that greengrass daemon is still alive
  daemon_cmdline=`cat /proc/$daemon_pid/cmdline`
- if [[ $daemon_cmdline != ^/greengrass/ggc/packages/1.8.1/bin/daemon.* ]]; then 
+ if [[ $daemon_cmdline != ^/greengrass/ggc/packages/$GG_VERSION/bin/daemon.* ]]; then 
   sleep 1;
  else
   break;
